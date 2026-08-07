3dcbac76bc8f2ef5175a3211fe970af14a68f879
Beta Display <noreply@users.noreply.github.com>
2026-08-07 16:51:16 +0800

    Update Cask for Beta Display 1.1.11

--- Casks/beta-display.rb
+++ Casks/beta-display.rb
@@ -1,9 +1,9 @@
 cask "beta-display" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.1.10"
-  sha256 arm:   "21559d9545228c8d45263b0b722b52c9c5d2b1bcac101b9e6075faa1ca870e89",
-         intel: "9628a50ba2c79e2b4c5145863bff72aece8648f0ca779617617d2159a97dab27"
+  version "1.1.11"
+  sha256 arm:   "2e88f8ae3bd3e4ce414210d9d5eaa7708f379b0bc86a60dfdaeed7620696deaa",
+         intel: "5bcea98de786713c5ee9a5f5cfcec7569d65f582c1e8d9fe429a2c7893c1c8ac"
 
   url "https://github.com/ysdj/beta-display/releases/download/v#{version}/BetaDisplay-#{version}-#{arch}.zip"
   name "Beta Display"
