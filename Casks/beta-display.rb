0fdc073ac86817533646b261b7c01036553ac55a
Beta Display <noreply@users.noreply.github.com>
2026-09-17 17:39:26 +0800

    Beta Display 1.1.14 Cask

--- Casks/beta-display.rb
+++ Casks/beta-display.rb
@@ -1,9 +1,9 @@
 cask "beta-display" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "1.1.13"
-  sha256 arm:   "ee85ec7e9e8b8d638f3f03ebf0fb1641547d126a2eae931d9261c5086e65a802",
-         intel: "096ed8f79ff540c338be69b575a43f8ffaacd5fd9584e8cc2af6c202e4c20a6d"
+  version "1.1.14"
+  sha256 arm:   "ba1b7c1afe1ac36baa346725b0447b2955ac3b76f09a169a697182b4ce50e697",
+         intel: "0954262e0af616d4384974cc6b668310c9ed3880876decbb11a17c313f4dace0"
 
   url "https://github.com/ysdj/beta-display/releases/download/v#{version}/BetaDisplay-#{version}-#{arch}.zip"
   name "Beta Display"
